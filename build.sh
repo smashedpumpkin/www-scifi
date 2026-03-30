537ddd967711e02a569ad407e8cc777dca80f3c7
smashedpumpkin <matt@wayouthere.co.uk>
2026-03-30 11:33:15 -0700

    minor change to build.sh to make it easier to verify contents

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@ set -e
 
 rm -f output/site.zip
 cargo run --release
-zip -j output/site.zip output/index.html
-echo "Zipped → output/site.zip"
+zip -j output/site.zip output/index.html content/icon-x-small.png
+
+echo ""
+echo "✓ Build complete"
+echo ""
+zipinfo output/site.zip
